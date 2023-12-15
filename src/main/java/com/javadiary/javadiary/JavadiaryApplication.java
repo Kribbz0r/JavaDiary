201d7cd1e291a26352ab198d984c2e1c24483e57
Kribbz0r <kribbz0r@gmail.com>
2023-12-15 22:31:07 +0100

    .

---
+++ src/main/java/com/javadiary/javadiary/JavadiaryApplication.java
@@ -0,0 +1,13 @@
+package com.javadiary.javadiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavadiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavadiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javadiary/javadiary/JavadiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadiary.javadiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavadiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
